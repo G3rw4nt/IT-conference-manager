5c30878f9c931c3e584fda5368d763e24cc250bc
G3rw4nt <krzysztof.dereszynski@gmail.com>
2022-05-07 12:23:25 +0200

    Initial commit

---
+++ src/main/java/pl/gerwant/itconferencemanager/ItConferenceManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.gerwant.itconferencemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConferenceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItConferenceManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gerwant/itconferencemanager/ItConferenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gerwant.itconferencemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
