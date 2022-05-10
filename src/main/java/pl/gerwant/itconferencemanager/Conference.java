cf4399d0c20f771532f3a0179230db09a1bc1b52
G3rw4nt <krzysztof.dereszynski@gmail.com>
2022-05-10 18:41:08 +0200

    code cleanup and ordering

--- src/main/java/pl/gerwant/itconferencemanager/Conference.java
+++ src/main/java/pl/gerwant/itconferencemanager/Conference.java
@@ -11,19 +11,19 @@ public class Conference {
 
 
     public Conference() {
-        this.date = LocalDate.of(2021,01,06);
-        lectures =  new ArrayList<>();
-        lectures.add(new Lecture("10_1",LocalTime.of(10,0),LocalTime.of(11,45),105));
-        lectures.add(new Lecture("10_2",LocalTime.of(10,0),LocalTime.of(11,45),105));
-        lectures.add(new Lecture("10_3",LocalTime.of(10,0),LocalTime.of(11,45),105));
+        this.date = LocalDate.of(2021, 01, 06);
+        lectures = new ArrayList<>();
+        lectures.add(new Lecture("10_1", LocalTime.of(10, 0), LocalTime.of(11, 45), 105));
+        lectures.add(new Lecture("10_2", LocalTime.of(10, 0), LocalTime.of(11, 45), 105));
+        lectures.add(new Lecture("10_3", LocalTime.of(10, 0), LocalTime.of(11, 45), 105));
 
-        lectures.add(new Lecture("12_1",LocalTime.of(12,0),LocalTime.of(13,45),105));
-        lectures.add(new Lecture("12_2",LocalTime.of(12,0),LocalTime.of(13,45),105));
-        lectures.add(new Lecture("12_3",LocalTime.of(12,0),LocalTime.of(13,45),105));
+        lectures.add(new Lecture("12_1", LocalTime.of(12, 0), LocalTime.of(13, 45), 105));
+        lectures.add(new Lecture("12_2", LocalTime.of(12, 0), LocalTime.of(13, 45), 105));
+        lectures.add(new Lecture("12_3", LocalTime.of(12, 0), LocalTime.of(13, 45), 105));
 
-        lectures.add(new Lecture("14_1",LocalTime.of(14,0),LocalTime.of(15,45),105));
-        lectures.add(new Lecture("14_2",LocalTime.of(14,0),LocalTime.of(15,45),105));
-        lectures.add(new Lecture("14_3",LocalTime.of(14,0),LocalTime.of(15,45),105));
+        lectures.add(new Lecture("14_1", LocalTime.of(14, 0), LocalTime.of(15, 45), 105));
+        lectures.add(new Lecture("14_2", LocalTime.of(14, 0), LocalTime.of(15, 45), 105));
+        lectures.add(new Lecture("14_3", LocalTime.of(14, 0), LocalTime.of(15, 45), 105));
 
     }
 
--- src/main/java/pl/gerwant/itconferencemanager/dao/entities/User.java
+++ src/main/java/pl/gerwant/itconferencemanager/dao/entities/User.java
@@ -1,6 +1,7 @@
 package pl.gerwant.itconferencemanager.dao.entities;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
